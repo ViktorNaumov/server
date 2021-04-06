9b45a350402433cc5901e86c6600052b6e4a72cc
ViktorNaumov <viktornaumov2011@mail.ru>
2021-04-06 14:36:28 +0300

    orderreduser2

--- controllers/getcontr.js
+++ controllers/getcontr.js
@@ -2,6 +2,7 @@ const thickness = require("../geters/getthickness");
 const steel = require("../geters/getsteel");
 const nameholder = require("../geters/getnameholder");
 const order = require("../geters/getorder")
+const ordercost = require("../geters/getordercost")
 
 module.exports.thickness = function (req, res) {
   thickness.getthickness((value) => {
@@ -32,10 +33,15 @@ module.exports.order = function (req, res) {
     if (value) {
       if(value[0] !== undefined){
         if(value[0].id )
-      res.json({ resultCode: 0, value: true });
+      res.json({ resultCode: 0, value: value[0] });
       } else{
         res.json({ resultCode: 1});  
       } 
     }
   });
 };
+
+module.exports.ordercost = function (req, res){
+
+}
+
